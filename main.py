180b07ded153285b786c07ee9ded65a6e206f485
Snehidh <98679394+Rinumodz@users.noreply.github.com>
2022-09-11 00:43:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from pyrogram import Client, filters
 
+
+API_ID = "15530355"
+API_HASH = "4ca71076aff9fb546651db89c9cae8b1"
+BOT_TOKEN = "5691093159:AAFORC2hJdHJfPW5P88QFSChFvNr0n-a6pM"
+
+
+RMMODS = Client(
+    name="18---Bot", 
+    api_id=API_ID, 
+    api_hash=API_HASH, 
+    bot_token=BOT_TOKEN
+) 
+
+
+print("Bot Was Started")
+
+RMMODS.run() 
